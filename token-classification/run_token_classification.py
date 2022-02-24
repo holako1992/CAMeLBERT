a952895f853489c1ce6c633b5df3f9a28ceb7cb2
holako1992 <62583292+holako1992@users.noreply.github.com>
2022-02-24 13:51:20 +0400

    Add files via upload

--- token-classification/run_token_classification.py
+++ token-classification/run_token_classification.py
@@ -308,7 +308,7 @@ def main():
         trainer.save_model()
         # For convenience, we also re-save the tokenizer to the same directory,
         # so that you can share your model easily on huggingface.co/models =)
-        if trainer.is_world_master():
+        if trainer.is_world_process_zero():
             tokenizer.save_pretrained(training_args.output_dir)
 
     # Evaluation
@@ -320,7 +320,7 @@ def main():
 
         output_eval_file = os.path.join(training_args.output_dir,
                                         "eval_results.txt")
-        if trainer.is_world_master():
+        if trainer.is_world_process_zero():
             with open(output_eval_file, "w") as writer:
                 logger.info("***** Eval results *****")
                 for key, value in result.items():
@@ -346,7 +346,7 @@ def main():
 
         output_test_results_file = os.path.join(training_args.output_dir,
                                                 "test_results.txt")
-        if trainer.is_world_master():
+        if trainer.is_world_process_zero():
             with open(output_test_results_file, "w") as writer:
                 for key, value in metrics.items():
                     logger.info("  %s = %s", key, value)
@@ -355,7 +355,7 @@ def main():
         # Save predictions
         output_test_predictions_file = os.path.join(training_args.output_dir,
                                                     "test_predictions.txt")
-        if trainer.is_world_master():
+        if trainer.is_world_process_zero():
             with open(output_test_predictions_file, "w") as writer:
                 with open(os.path.join(data_args.data_dir, "test.txt"), "r") as f:
                     example_id = 0
